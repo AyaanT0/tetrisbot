76c5f4b23e19ef104eb8083c7a49b80f6a4e4fe0
Ayaan Tunio <76827380+AyaanT0@users.noreply.github.com>
2022-03-14 19:17:50 -0400

    Update tetrisbot.py

--- tetrisbot.py
+++ tetrisbot.py
@@ -437,7 +437,7 @@ async def on_message(message):
 
 
     if message.content.lower().startswith(">invite"):
-        await message.channel.send("https://discord.com/oauth2/authorize?client_id=951289283661156393&scope=bot&permissions=11264")
+        await message.channel.send("https://discord.com/oauth2/authorize?client_id=951289283661156393&scope=bot&permissions=11328")
 
 
     if message.content.lower().startswith(">leaderboard"):
